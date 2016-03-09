b4051383255dc6fc08e9fc0109e3fd76d3076494
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-09 22:14:29 +0530

    Added Console App with Selenium integration for Chrome

---
+++ src/Sharp48.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Sharp48.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var driverPath = Path.Combine(Environment.CurrentDirectory);
+            var options = new ChromeOptions();
+            IWebDriver driver = new ChromeDriver(driverPath, options);
+
+            driver.Navigate().GoToUrl("http://google.com?q=cheese");
+
+            //Close the browser
+            driver.Quit();
+
+            // Wait
+            Console.ReadLine();
+        }
+    }
+}
