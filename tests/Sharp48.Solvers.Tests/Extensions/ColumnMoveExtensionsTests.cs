345d069058fc6d630eb5d38d2ef15da1f1e5dc38
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-10 18:29:02 +0530

    Refactored tests for ISquare-based extensions

--- tests/Sharp48.Solvers.Tests/Extensions/ColumnMoveExtensionsTests.cs
+++ tests/Sharp48.Solvers.Tests/Extensions/ColumnMoveExtensionsTests.cs
@@ -15,25 +15,6 @@ namespace Sharp48.Solvers.Tests.Extensions
         [InlineData("2,2,,2", true)]
         [InlineData(",,,2", false)]
         [InlineData(",,4,2", false)]
-        public void RightMoveAvailabilityWorks(string column, bool expected)
-        {
-            // Arrange
-            var squares = Column.Parse(column);
-
-            // Act
-            var actual = squares.IsRightMovePossible();
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Theory]
-        [InlineData("2,,,", true)]
-        [InlineData("2,2,,", true)]
-        [InlineData("2,,,2", true)]
-        [InlineData("2,2,,2", true)]
-        [InlineData(",,,2", false)]
-        [InlineData(",,4,2", false)]
         public void DownMoveExists(string column, bool exists)
         {
             // Arrange
@@ -63,48 +44,6 @@ namespace Sharp48.Solvers.Tests.Extensions
 
             // Assert
             Assert.Equal(exists, actual);
-        }
-
-        [Theory]
-        [InlineData("2,,,", " , , ,2")]
-        [InlineData("2,2,,", " , ,2,2")]
-        [InlineData("2,,,2", " , ,2,2")]
-        [InlineData("2,2,,2", " ,2,2,2")]
-        [InlineData(",,,2", " , , ,2")]
-        [InlineData(",,4,2", " , ,4,2")]
-        public void SlideRightWorks(string input, string expected)
-        {
-            // Arrange
-            var squares = Column.Parse(input);
-
-            // Act
-            var slided = new Column(squares.SlideRight().ToList());
-            var actual = slided.ToString();
-
-            // Assert
-            Assert.Equal(expected, actual);
-
-        }
-
-        [Theory]
-        [InlineData("2,,,", "2, , , ")]
-        [InlineData("2,2,,", " ,4, , ")]
-        [InlineData("2,,,2", "2, , ,2")]
-        [InlineData("2,2,,2", " ,4, ,2")]
-        [InlineData(",,,2", " , , ,2")]
-        [InlineData(",,4,2", " , ,4,2")]
-        public void MergeRightWorks(string input, string expected)
-        {
-            // Arrange
-            var squares = Column.Parse(input);
-            uint score;
-
-            // Act
-            var slided = new Column(squares.MergeRight(out score).ToList());
-            var actual = slided.ToString();
-
-            // Assert
-            Assert.Equal(expected, actual);
         }
     }
 }
---
+++ tests/Sharp48.Solvers.Tests/Extensions/SquareCollectionMoveExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using Sharp48.Core.PlayArea;
+using Sharp48.Solvers.Extensions;
+using Xunit;
+
+namespace Sharp48.Solvers.Tests.Extensions
+{
+    public class SquareCollectionMoveExtensionsTests
+    {
+
+        [Theory]
+        [InlineData("2,,,", true)]
+        [InlineData("2,2,,", true)]
+        [InlineData("2,,,2", true)]
+        [InlineData("2,2,,2", true)]
+        [InlineData(",,,2", false)]
+        [InlineData(",,4,2", false)]
+        public void RightMoveAvailabilityWorks(string row, bool expected)
+        {
+            // Arrange
+            var squares = Row.Parse(row);
+
+            // Act
+            var actual = squares.IsRightMovePossible();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("2,,,", " , , ,2")]
+        [InlineData("2,2,,", " , ,2,2")]
+        [InlineData("2,,,2", " , ,2,2")]
+        [InlineData("2,2,,2", " ,2,2,2")]
+        [InlineData(",,,2", " , , ,2")]
+        [InlineData(",,4,2", " , ,4,2")]
+        public void SlideRightWorks(string input, string expected)
+        {
+            // Arrange
+            var squares = Row.Parse(input);
+
+            // Act
+            var slided = new Row(squares.SlideRight().ToList());
+            var actual = slided.ToString();
+
+            // Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData("2,,,", "2, , , ")]
+        [InlineData("2,2,,", " ,4, , ")]
+        [InlineData("2,,,2", "2, , ,2")]
+        [InlineData("2,2,,2", " ,4, ,2")]
+        [InlineData(",,,2", " , , ,2")]
+        [InlineData(",,4,2", " , ,4,2")]
+        public void MergeRightWorks(string input, string expected)
+        {
+            // Arrange
+            var squares = Row.Parse(input);
+            uint score;
+
+            // Act
+            var slided = new Row(squares.MergeRight(out score).ToList());
+            var actual = slided.ToString();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
