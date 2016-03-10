6095f8f2fed6e8091a8b954c68f7c5a5bdfe9290
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-10 13:32:54 +0530

    Added move extension tests for row

---
+++ tests/Sharp48.Solvers.Tests/Extensions/RowMoveExtensionsTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using Sharp48.Core.Moves;
+using Sharp48.Core.PlayArea;
+using Sharp48.Solvers.Extensions;
+using Xunit;
+
+namespace Sharp48.Solvers.Tests.Extensions
+{
+    public class RowMoveExtensionsTests
+    {
+        [Theory]
+        [InlineData("2,,,", true)]
+        [InlineData("2,2,,", true)]
+        [InlineData("2,,,2", true)]
+        [InlineData("2,2,,2", true)]
+        [InlineData(",,,2", false)]
+        [InlineData(",,4,2", false)]
+        public void RightMoveAvailabilityWorks(string row, bool expected)
+        {
+            // Arrange
+            var squares = Row.Parse(row);
+
+            // Act
+            var actual = squares.IsRightMovePossible();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("2,,,", true)]
+        [InlineData("2,2,,", true)]
+        [InlineData("2,,,2", true)]
+        [InlineData("2,2,,2", true)]
+        [InlineData(",,,2", false)]
+        [InlineData(",,4,2", false)]
+        public void RightMoveExists(string row, bool exists)
+        {
+            // Arrange
+            var squares = Row.Parse(row);
+
+            // Act
+            var actual = squares.GetPossibleMoves().Contains(Move.Right);
+
+            // Assert
+            Assert.Equal(exists, actual);
+        }
+
+        [Theory]
+        [InlineData("2,,,", false)]
+        [InlineData("2,2,,", true)]
+        [InlineData("2,,,2", true)]
+        [InlineData("2,2,,2", true)]
+        [InlineData(",,,2", true)]
+        [InlineData(",,4,2", true)]
+        public void LeftMoveExists(string row, bool exists)
+        {
+            // Arrange
+            var squares = Row.Parse(row);
+
+            // Act
+            var actual = squares.GetPossibleMoves().Contains(Move.Left);
+
+            // Assert
+            Assert.Equal(exists, actual);
+        }
+
+    }
+}
--- tests/Sharp48.Solvers.Tests/Properties/AssemblyInfo.cs
+++ tests/Sharp48.Solvers.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Sharp48.Solvers")]
+[assembly: AssemblyTitle("Sharp48.Solvers.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sharp48.Solvers")]
+[assembly: AssemblyProduct("Sharp48.Solvers.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,12 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("3de6f17c-ed2d-44f7-b7db-1c49e951f08a")]
-
-// For Unit Tests
-[assembly:InternalsVisibleTo("Sharp48.Solvers.Tests")]
+[assembly: Guid("7b197aa5-826e-467a-93b2-ea807f90fde3")]
