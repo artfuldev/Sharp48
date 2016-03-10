629aa8355cd63fbf364b4b620e1fe9b9162ed5a5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-10 11:58:43 +0530

    Added Intelligent Solver

---
+++ src/Sharp48.Solvers/IntelligentSolver.cs
@@ -0,0 +1,13 @@
+using Sharp48.Core;
+using Sharp48.Core.Moves;
+
+namespace Sharp48.Solvers
+{
+    public class IntelligentSolver : ISolver
+    {
+        public Move GetBestMove(IGame game)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
