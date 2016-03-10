e41cbfa56178d7c04446a787d6419c7ba35214fe
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-10 13:22:31 +0530

    Added tests for parsing

---
+++ tests/Sharp48.Core.Tests/RowTests.cs
@@ -0,0 +1,22 @@
+﻿using Sharp48.Core.PlayArea;
+using Xunit;
+
+namespace Sharp48.Core.Tests
+{
+    public class RowTests
+    {
+        [Theory]
+        [InlineData("2,,,","2, , , ")]
+        public void ParsingWorks(string input, string expected)
+        {
+            // Arrange
+
+            // Act
+            var row = Row.Parse(input);
+            var actual = row.ToString();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
