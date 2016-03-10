18a7262c2ec3cc03bafb74988b7bdbfc553da495
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-10 13:12:53 +0530

    Added Row and Column Parsers

--- src/Sharp48.Core/PlayArea/Column.cs
+++ src/Sharp48.Core/PlayArea/Column.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using Sharp48.Core.Tiles;
 
 namespace Sharp48.Core.PlayArea
 {
@@ -8,6 +9,19 @@ namespace Sharp48.Core.PlayArea
     {
         public Column(IList<ISquare> list) : base(list)
         {
+        }
+        public static Column Parse(string input)
+        {
+            var squares = Enumerable.Range(1, 4).Select(x => (ISquare)new Square()).ToList();
+            var tiles = input.Split(',')
+                .Select(x => string.IsNullOrWhiteSpace(x) ? (ITile)null : new Tile { Value = uint.Parse(x) })
+                .ToArray();
+            for (var i = 0; i < tiles.Length; i++)
+            {
+                if (tiles[i] != null)
+                    squares[i].Tile = tiles[i];
+            }
+            return new Column(squares);
         }
 
         public override string ToString()
