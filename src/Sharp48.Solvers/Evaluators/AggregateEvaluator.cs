7cec28fdd98b14933b645714659445887a2fd422
Sudarsan Balaji <sudarsan.balaji@outlook.com>
2016-03-12 23:34:29 +0530

    Added Aggregate and Smoothness evaluators

---
+++ src/Sharp48.Solvers/Evaluators/AggregateEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sharp48.Core;
+
+namespace Sharp48.Solvers.Evaluators
+{
+    public class AggregateEvaluator : ICacheableEvaluator
+    {
+        private readonly IEnumerable<IEvaluator> _evaluators;
+
+        public AggregateEvaluator(IEnumerable<IEvaluator> evaluators)
+        {
+            _evaluators = evaluators;
+        }
+
+        public double Evaluate(IGame game)
+            => _evaluators.Aggregate(0D, (current, next) => current + next.Evaluate(game));
+
+        public string GetCacheKey(IGame game) => game.Grid.ToString();
+    }
+}
---
+++ src/Sharp48.Solvers/Evaluators/SmoothnessEvaluator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sharp48.Core;
+using Sharp48.Core.PlayArea;
+using Sharp48.Solvers.Extensions;
+
+namespace Sharp48.Solvers.Evaluators
+{
+    public class SmoothnessEvaluator : ICacheableEvaluator
+    {
+        private readonly IDictionary<string, double> _squaresScores = new Dictionary<string, double>();
+
+        private double Evaluate(IEnumerable<ISquare> squares)
+        {
+            var array = squares.Select(x=>x.GetSafeTileValue()).ToArray();
+            var key = string.Join(",", array);
+            if (_squaresScores.ContainsKey(key))
+                return _squaresScores[key];
+            var score = 0d;
+            for (var i = 1; i < array.Length - 1; i++)
+                score -= Math.Abs(array[i - 1] - array[i]) + Math.Abs(array[i + 1] - array[i]);
+            return _squaresScores[key] = score;
+        }
+
+        public double Evaluate(IGame game)
+        {
+            return game.Grid.Columns.Aggregate(
+                game.Grid.Rows.Aggregate(0d, (current, next) => current + Evaluate(next)),
+                (current, next) => current + Evaluate(next));
+        }
+
+        public string GetCacheKey(IGame game) => game.Grid.ToString();
+    }
+}
