f2a33179f23c6a98045ab84a252f3806a515c8dc
Sudarsan Balaji <sudarsan.balaji@outlook.com>
2016-03-12 22:50:34 +0530

    Added Expectimax Evaluator. Fixes #1

---
+++ src/Sharp48.Solvers/Evaluators/ExpectimaxEvaluator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sharp48.Core;
+using Sharp48.Solvers.Extensions;
+
+namespace Sharp48.Solvers.Evaluators
+{
+    public class ExpectimaxEvaluator : IEvaluator
+    {
+        private readonly IEvaluator _evaluator = new ScoreEvaluator();
+        private readonly IDictionary<string, double> _hashTable = new Dictionary<string, double>();
+
+        public double Evaluate(IGame game) => ExpectiMaxScore(game, CalculateDepth(game));
+
+        private double Score(IGame game)
+        {
+            var key = game.Grid.ToString();
+            if (!_hashTable.ContainsKey(key))
+                _hashTable[key] = _evaluator.Evaluate(game);
+            return _hashTable[key];
+        }
+
+        private double ExpectiMaxScore(IGame game, byte depth)
+        {
+            if (game.Over || depth == 0)
+                return Score(game);
+            double alpha;
+            // Random event at node
+            if (depth%2 == 0)
+            {
+                var possibleGenerations = game.GetPossibleGenerations().ToList();
+                var probability = (double) 1/possibleGenerations.Count;
+                alpha = possibleGenerations.Sum(node => probability*ExpectiMaxScore(node, (byte) (depth - 1)));
+            }
+            // If we are to play at node
+            else
+            {
+                var possibleGames = game.GetPossibleMoves().Select(game.MakeMove);
+                alpha = possibleGames.Max(x => ExpectiMaxScore(x, (byte) (depth - 1)));
+            }
+            return alpha;
+        }
+
+        private static byte CalculateDepth(IGame game) => 4;
+    }
+}
--- src/Sharp48.Solvers/IntelligentSolver.cs
+++ src/Sharp48.Solvers/IntelligentSolver.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using Sharp48.Core;
 using Sharp48.Core.Moves;
@@ -11,47 +9,15 @@ namespace Sharp48.Solvers
 {
     public class IntelligentSolver : ISolver
     {
-        private readonly IDictionary<string, double> _hashTable = new Dictionary<string, double>();
-        private readonly IEvaluator _evaluator = new ScoreEvaluator();
+        private readonly IEvaluator _evaluator = new ExpectimaxEvaluator();
 
         public Move GetBestMove(IGame game)
         {
-            var n = 2*Math.Log10(game.Grid.Squares.Max(x => x.GetSafeTileValue()))/Math.Log10(2);
-            var depth = Convert.ToByte(n > 4 ? 4 : n);
             var possibleMoves = game.GetPossibleMoves();
             var movesToScoreDictionary = possibleMoves.ToDictionary(x => x,
-                x => ExpectiMaxScore(game.MakeMove(x), depth));
+                x => _evaluator.Evaluate(game.MakeMove(x)));
             var bestScore = movesToScoreDictionary.Max(x => x.Value);
             return movesToScoreDictionary.First(x => Math.Abs(x.Value - bestScore) < 0.1).Key;
-        }
-
-        public double Score(IGame game)
-        {
-            var key = game.Grid.ToString();
-            if (!_hashTable.ContainsKey(key))
-                _hashTable[key] = _evaluator.Evaluate(game);
-            return _hashTable[key];
-        }
-
-        public double ExpectiMaxScore(IGame game, byte depth)
-        {
-            if (game.Over || depth == 0)
-                return Score(game);
-            double alpha;
-            // Random event at node
-            if (depth%2 == 0)
-            {
-                var possibleGenerations = game.GetPossibleGenerations().ToList();
-                var probability = (double) 1/possibleGenerations.Count;
-                alpha = possibleGenerations.Sum(node => probability*ExpectiMaxScore(node, (byte) (depth - 1)));
-            }
-            // If we are to play at node
-            else
-            {
-                var possibleGames = game.GetPossibleMoves().Select(game.MakeMove);
-                alpha = possibleGames.Max(x => ExpectiMaxScore(x, (byte) (depth - 1)));
-            }
-            return alpha;
         }
     }
 }
