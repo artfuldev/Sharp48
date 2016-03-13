ac651cbaec4a97b2c4ed01f46c0400c9fcf2d8f8
Sudarsan Balaji <sudarsan.balaji@outlook.com>
2016-03-13 21:50:41 +0530

    Added EmptyTileEvaluator

---
+++ src/Sharp48.Solvers/Evaluators/RowEvaluation/EmptyTileEvaluator.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace Sharp48.Solvers.Evaluators.RowEvaluation
+{
+    public class EmptyTileEvaluator : RowEvaluatorBase
+    {
+        protected override double EvaluateImplementation(uint[] tiles) => tiles.Count(x => x == 0);
+    }
+}
