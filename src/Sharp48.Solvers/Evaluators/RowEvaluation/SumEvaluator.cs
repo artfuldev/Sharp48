0847b394e75a02aa08949695b91d126bf291599b
Sudarsan Balaji <sudarsan.balaji@outlook.com>
2016-03-13 22:04:12 +0530

    Edited out comment

--- src/Sharp48.Solvers/Evaluators/RowEvaluation/SumEvaluator.cs
+++ src/Sharp48.Solvers/Evaluators/RowEvaluation/SumEvaluator.cs
@@ -6,7 +6,7 @@ namespace Sharp48.Solvers.Evaluators.RowEvaluation
     public class SumEvaluator : RowEvaluatorBase
     {
         private readonly double _factor;
-        // 3.5
+
         public SumEvaluator(double factor)
         {
             _factor = factor;
