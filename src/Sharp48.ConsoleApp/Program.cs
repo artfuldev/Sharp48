3fab9ca31c3a836c6a113bfe852893ab2223df1e
Sudarsan Balaji <sudarsan.balaji@outlook.com>
2016-03-13 21:58:28 +0530

    Added MergeEvaluator

--- src/Sharp48.ConsoleApp/Program.cs
+++ src/Sharp48.ConsoleApp/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Sharp48.ConsoleApp.Logging;
 using Sharp48.Solvers;
 using Sharp48.Solvers.Evaluators;
+using Sharp48.Solvers.Evaluators.RowEvaluation;
 using Sharp48.UserInterfaces;
 
 namespace Sharp48.ConsoleApp
@@ -17,15 +18,12 @@ namespace Sharp48.ConsoleApp
             var logger = new NullLogger();
             var evaluators = new List<IEvaluator>()
             {
-                new TransformEvaluator(new FreeSpaceEvaluator(), (score) => Math.Pow(score,4)),
-                new TransformEvaluator(new SmoothnessEvaluator(), (score) => score),
-                new TransformEvaluator(new MonotonyEvaluator(), (score) => 32768*score),
-                new TransformEvaluator(new MaxTileEvaluator(), (score) => score*score),
-                new TransformEvaluator(new CornerMaxTileEvaluator(), (score) => 32768*32768*score),
-                new TransformEvaluator(new PotentialMergesEvaluator(), (score) => score*score),
+                new TransformEvaluator(new RowScoreEvaluator(3.5), (score) => 11*score),
+                new TransformEvaluator(new EmptyTileEvaluator(), (score) => 270*score),
+                new TransformEvaluator(new MergeEvaluator(), (score) => 700*score),
                 new Reaching2048IsAWinEvaluator(),
             };
-            var mainEvaluator = new ExpectimaxEvaluator(new CachingEvaluator(new AggregateEvaluator(evaluators)), 3);
+            var mainEvaluator = new ExpectimaxEvaluator(new CachingEvaluator(new AggregateEvaluator(evaluators)), 4);
             var solver = new IntelligentSolver(mainEvaluator);
             using (var runner = new GameRunner(ui, solver, logger))
                 runner.Run();
---
+++ src/Sharp48.Solvers/Evaluators/RowEvaluation/MergeEvaluator.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace Sharp48.Solvers.Evaluators.RowEvaluation
+{
+    public class MergeEvaluator : RowEvaluatorBase
+    {
+        protected override double EvaluateImplementation(uint[] tiles)
+        {
+            var merges = 0;
+            var prev = 0u;
+            var counter = 0;
+            foreach (var tile in tiles.Where(x => x != 0))
+            {
+                if (prev == tile)
+                    counter++;
+                else if (counter > 0)
+                {
+                    merges += 1 + counter;
+                    counter = 0;
+                }
+                prev = tile;
+            }
+            if (counter > 0)
+            {
+                merges += 1 + counter;
+            }
+            return merges;
+        }
+    }
+}
