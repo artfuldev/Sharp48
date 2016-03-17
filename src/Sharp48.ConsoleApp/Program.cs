e54c6ea7e8ca9202a43b1c0089a7a1137227bd0a
Sudarsan Balaji <sudarsan.balaji@outlook.com>
2016-03-18 01:20:43 +0530

    Still trying

--- src/Sharp48.ConsoleApp/Program.cs
+++ src/Sharp48.ConsoleApp/Program.cs
@@ -16,11 +16,11 @@ namespace Sharp48.ConsoleApp
                 new IntelligentSolver(
                     new ExpectimaxEvaluator(new SumEvaluator(new List<IEvaluator>()
                     {
-                        new TransformEvaluator(new TileSumEvaluator(2), (score) => score),
-                        new TransformEvaluator(new EmptyTileEvaluator(), (score) => 32768*score),
-                        new TransformEvaluator(new MergeEvaluator(), (score) => Math.Pow(2, score)),
-                        new TransformEvaluator(new MonotonicityEvaluator(2), (score) => score),
-                        new TransformEvaluator(new SmoothnessEvaluator(2), (score) => score)
+                        //new TransformEvaluator(new TileSumEvaluator(2), (score) => score),
+                        new TransformEvaluator(new EmptyTileEvaluator(), (score) => Math.Pow(15, score)),
+                        new TransformEvaluator(new MergeEvaluator(), (score) => Math.Pow(7, score)),
+                        new TransformEvaluator(new MonotonicityEvaluator(15), (score) => score),
+                        //new TransformEvaluator(new SmoothnessEvaluator(2), (score) => score)
                     })));
             var ui = new GoogleChromeUI(Path.Combine(Environment.CurrentDirectory));
             using (var runner = new GameRunner(ui, solver))
