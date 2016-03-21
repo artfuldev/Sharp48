a13de9111951c9a570f8b3c29d17e94c85e31534
Sudarsan Balaji <sudarsan.balaji@outlook.com>
2016-03-21 23:56:56 +0530

    Adding SnakeEvaluator

---
+++ src/Sharp48.Solvers/Evaluators/SnakeEvaluator.cs
@@ -0,0 +1,12 @@
+﻿using Sharp48.Core;
+
+namespace Sharp48.Solvers.Evaluators
+{
+    public class SnakeEvaluator : IEvaluator
+    {
+        public double Evaluate(IGame game)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
