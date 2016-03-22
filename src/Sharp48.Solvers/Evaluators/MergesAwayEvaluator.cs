d227ef97d32a8d6fdf134c59db8aa789fa9223e5
Sudarsan Balaji <sudarsan.balaji@outlook.com>
2016-03-22 21:59:44 +0530

    Fixed 2 showing merges away

--- src/Sharp48.Solvers/Evaluators/MergesAwayEvaluator.cs
+++ src/Sharp48.Solvers/Evaluators/MergesAwayEvaluator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using Sharp48.Core;
 using Sharp48.Solvers.Extensions;
@@ -7,19 +8,19 @@ namespace Sharp48.Solvers.Evaluators
 {
     public class MergesAwayEvaluator : IEvaluator
     {
-        private readonly IDictionary<uint, double> _mergesAway = new Dictionary<uint, double>();
+        private readonly IDictionary<uint, double> _mergesAway = new ConcurrentDictionary<uint, double>();
 
         public double Evaluate(IGame game)
             =>
                 game.Grid.Squares.Select(x => x.GetSafeTileValue())
-                    .Where(x => x != 0)
+                    .Where(x => x > 2)
                     .Sum(x => -MergesAway(x));
 
         private double MergesAway(uint tile)
         {
-            if (_mergesAway.ContainsKey(tile))
-                return _mergesAway[tile];
-            return _mergesAway[tile] = tile == 2 ? 1 : 2*MergesAway(tile/2);
+            return _mergesAway.ContainsKey(tile)
+                ? _mergesAway[tile]
+                : (_mergesAway[tile] = tile == 4 ? 1 : 2*MergesAway(tile/2));
         }
     }
 }
