68c5e82398c0a8b542f77f863695868de7e794e5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-04-02 23:40:32 +0530

    Adding more transformation

--- src/Sharp48.ConsoleApp/Program.cs
+++ src/Sharp48.ConsoleApp/Program.cs
@@ -17,7 +17,7 @@ namespace Sharp48.ConsoleApp
                     new ExpectimaxEvaluator(new CachingEvaluator(new SumEvaluator(new List<IEvaluator>
                     {
                         new TransformEvaluator(new EmptyTileEvaluator(), (score) => 2048*score),
-                        new MergeEvaluator(),
+                        new TransformEvaluator(new MergeEvaluator(), (score) => score/2048),
                         new MergesAwayEvaluator(),
                         new Reaching2048IsAWinEvaluator(),
                     }))));
