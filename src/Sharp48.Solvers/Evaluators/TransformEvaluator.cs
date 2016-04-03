e1e78e01e427a8a7ab15902dc3c8bbdcb39aa849
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-04-04 00:23:17 +0530

    Added game to transform function

--- src/Sharp48.Solvers/Evaluators/TransformEvaluator.cs
+++ src/Sharp48.Solvers/Evaluators/TransformEvaluator.cs
@@ -6,14 +6,14 @@ namespace Sharp48.Solvers.Evaluators
     public class TransformEvaluator : IEvaluator
     {
         private readonly IEvaluator _evaluator;
-        private readonly Func<double, double> _transformFunc;
+        private readonly Func<double, IGame, double> _transformFunc;
 
-        public TransformEvaluator(IEvaluator evaluator, Func<double, double> transformFunc)
+        public TransformEvaluator(IEvaluator evaluator, Func<double, IGame, double> transformFunc)
         {
             _evaluator = evaluator;
             _transformFunc = transformFunc;
         }
 
-        public double Evaluate(IGame game) => _transformFunc(_evaluator.Evaluate(game));
+        public double Evaluate(IGame game) => _transformFunc(_evaluator.Evaluate(game), game);
     }
 }
