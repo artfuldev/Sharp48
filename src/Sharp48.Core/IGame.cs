165d801ab77d23d581563315e61644c870a5257c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-04-14 23:02:12 +0530

    Added comments

--- src/Sharp48.Core/IGame.cs
+++ src/Sharp48.Core/IGame.cs
@@ -2,10 +2,24 @@
 
 namespace Sharp48.Core
 {
+    /// <summary>
+    ///     Represents a game of 2048
+    /// </summary>
     public interface IGame
     {
+        /// <summary>
+        ///     The grid of the game.
+        /// </summary>
         IGrid Grid { get; }
+
+        /// <summary>
+        ///     Describes whether the game is over.
+        /// </summary>
         bool Over { get; }
+
+        /// <summary>
+        ///     The last generated tile's value.
+        /// </summary>
         byte LastGeneratedTileValue { get; }
     }
 }
