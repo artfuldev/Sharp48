26d370c3b7a167f320ccbeeb32b7da89a6d2a47c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-04-15 23:03:12 +0530

    Added comments to tiles

--- src/Sharp48.Core/Tiles/ITile.cs
+++ src/Sharp48.Core/Tiles/ITile.cs
@@ -1,7 +1,13 @@
 ﻿namespace Sharp48.Core.Tiles
 {
+    /// <summary>
+    ///     Represents a tile of the 2048 game.
+    /// </summary>
     public interface ITile
     {
+        /// <summary>
+        ///     The value of the tile as an unsigned integer. This is the same as the number displayed on the tile.
+        /// </summary>
         uint Value { get; }
     }
 }
--- src/Sharp48.Core/Tiles/Tile.cs
+++ src/Sharp48.Core/Tiles/Tile.cs
@@ -1,7 +1,13 @@
 ﻿namespace Sharp48.Core.Tiles
 {
+    /// <summary>
+    ///     Represents a tile of the 2048 game.
+    /// </summary>
     public class Tile : ITile
     {
+        /// <summary>
+        ///     The value of the tile as an unsigned integer. This is the same as the number displayed on the tile.
+        /// </summary>
         public uint Value { get; set; } = 2;
 
         public override string ToString()
