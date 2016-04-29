66737ae61aea56160a5d3cb32eeea3ea2eb17afa
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-04-29 21:39:12 +0530

    Added comments

--- src/Sharp48.Core/Game.cs
+++ src/Sharp48.Core/Game.cs
@@ -2,15 +2,30 @@
 
 namespace Sharp48.Core
 {
+    /// <summary>
+    /// Represents a game of 2048.
+    /// </summary>
     public class Game : IGame
     {
+        /// <summary>
+        /// Instantiates a <seealso cref="Game"/> with a grid and a game over flag.
+        /// </summary>
+        /// <param name="grid">The grid of the game.</param>
+        /// <param name="over">The game over status of the game.</param>
         public Game(IGrid grid, bool over)
         {
             Grid = grid;
             Over = over;
         }
 
+        /// <summary>
+        ///     The grid of the game.
+        /// </summary>
         public IGrid Grid { get; }
+
+        /// <summary>
+        ///     Describes whether the game is over.
+        /// </summary>
         public bool Over { get; }
     }
 }
