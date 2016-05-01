35fef8a45f59c702d6b902a60caaf45441f24c64
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-05-01 21:39:12 +0530

    Added comments

--- src/Sharp48.Core/PlayArea/ISquare.cs
+++ src/Sharp48.Core/PlayArea/ISquare.cs
@@ -2,8 +2,14 @@
 
 namespace Sharp48.Core.PlayArea
 {
+    /// <summary>
+    ///     Represents a square in the 2048 game grid.
+    /// </summary>
     public interface ISquare
     {
+        /// <summary>
+        ///     The tile, if any, present in the square.
+        /// </summary>
         ITile Tile { get; set; }
     }
 }
