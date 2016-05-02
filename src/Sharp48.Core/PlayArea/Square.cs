b7a577457e4b069da5b0636b87a729f1fa5630f4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-05-02 21:39:12 +0530

    Added comments

--- src/Sharp48.Core/PlayArea/Square.cs
+++ src/Sharp48.Core/PlayArea/Square.cs
@@ -2,10 +2,20 @@
 
 namespace Sharp48.Core.PlayArea
 {
+    /// <summary>
+    ///     Represents a square in the game grid of 2048.
+    /// </summary>
     public class Square : ISquare
     {
+        /// <summary>
+        ///     The tile, if any, present in the square.
+        /// </summary>
         public ITile Tile { get; set; } = null;
 
+        /// <summary>
+        ///     The string representation of a square. Allows for easier debugging.
+        /// </summary>
+        /// <returns>The string representation of a square, with its tile.</returns>
         public override string ToString()
         {
             return Tile?.ToString() ?? " ";
